28049ebd467d1f846d8be811507cfc72af55fd53
Bhanu Pratap Singh <info2bhanusingh@gmail.com>
2026-08-19 16:51:50 +0530

    Implemented ColumnGenerator in SchooClass

--- modules/web/src/com/company/schoolmanagement/web/screens/schoolclass/SchoolClassBrowse.java
+++ modules/web/src/com/company/schoolmanagement/web/screens/schoolclass/SchoolClassBrowse.java
@@ -1,11 +1,27 @@
 package com.company.schoolmanagement.web.screens.schoolclass;
 
+import com.haulmont.cuba.gui.UiComponents;
+import com.haulmont.cuba.gui.components.Button;
+import com.haulmont.cuba.gui.components.Component;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.schoolmanagement.entity.SchoolClass;
+
+import javax.inject.Inject;
 
 @UiController("schoolmanagement_SchoolClass.browse")
 @UiDescriptor("school-class-browse.xml")
 @LookupComponent("schoolClassesTable")
 @LoadDataBeforeShow
 public class SchoolClassBrowse extends StandardLookup<SchoolClass> {
+
+    @Inject
+    private UiComponents uiComponents;
+    @Install(to = "schoolClassesTable.action", subject = "columnGenerator")
+    private Component schoolClassesTableActionColumnGenerator(SchoolClass schoolClass) {
+        Button button = uiComponents.create(Button.class);
+        button.setCaption("Enroll");
+
+        return button;
+    }
+
 }
