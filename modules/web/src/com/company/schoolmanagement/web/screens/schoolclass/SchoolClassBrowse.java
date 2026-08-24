d5f43265c1215530b7adc71db980719a8aa7df4b
Bhanu Pratap Singh <info2bhanusingh@gmail.com>
2026-08-24 16:15:29 +0530

    Added the JPQL query for School Class Browse update when a school is selected, and centered the table elements

--- modules/web/src/com/company/schoolmanagement/web/screens/schoolclass/SchoolClassBrowse.java
+++ modules/web/src/com/company/schoolmanagement/web/screens/schoolclass/SchoolClassBrowse.java
@@ -5,6 +5,7 @@ import com.haulmont.cuba.gui.UiComponents;
 import com.haulmont.cuba.gui.components.GroupTable;
 import com.haulmont.cuba.gui.components.HasValue;
 import com.haulmont.cuba.gui.components.LookupPickerField;
+import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.schoolmanagement.entity.SchoolClass;
 
@@ -26,6 +27,9 @@ public class SchoolClassBrowse extends StandardLookup<SchoolClass> {
     @Inject
     private LookupPickerField<School> schoolFilter;
 
+    @Inject
+    private CollectionLoader<SchoolClass> schoolClassesDl;
+
     @Install(to = "schoolClassesTable.studentCount", subject = "valueProvider")
     private String schoolClassesTableStudentCountValueProvider(SchoolClass schoolClass) {
         return schoolClass.getStudents().size() + "/" + schoolClass.getCapacity();
@@ -35,17 +39,10 @@ public class SchoolClassBrowse extends StandardLookup<SchoolClass> {
     protected void onSchoolFilterValueChange(
             HasValue.ValueChangeEvent<School> event) {
 
-        Collection<SchoolClass> classes = schoolClassesTable.getItems().getItems();
-        School selectedSchool = schoolFilter.getValue();
+        School selectedSchool = event.getValue();
 
-        for(SchoolClass schoolClass :classes){
-            if(schoolClass.getSchool().equals(selectedSchool)){
-                schoolClassesTable.setVisible(false);
-            }
-        }
-
-
-        // your filtering logic here
+        schoolClassesDl.setParameter("school", selectedSchool);
+        schoolClassesDl.load();
     }
 
 
